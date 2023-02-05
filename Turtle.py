72ff1ae0fea36d6c625ef86a59652ae6570d772c
Lorenz <lorenzm2311@gmail.com>
2023-02-05 13:29:12 +0100

    push von den alten arbeiten

---
+++ Turtle.py
@@ -0,0 +1,8 @@
+import turtle
+turtle.color('yellow')
+turtle.circle(100)
+turtle.up()
+turtle.color('orange')
+turtle.forward(150)
+turtle.down()
+turtle.circle(100)
---
+++ hangman.py
@@ -0,0 +1,37 @@
+import random
+#Eingaben
+woerter = 'hund katze maus haus raus'.split()
+erraten = []
+nutzereingabe = ""
+fehlversuche = 8
+
+#Initialisierung
+rate_wort = random.choice(woerter)
+print(rate_wort)
+for buchstaben in rate_wort:
+    erraten.append('_')
+
+#Hauptspiel
+while nutzereingabe != "bye":
+    for ausgabe in erraten:
+        print(ausgabe, end=" ")
+    print()
+    nutzereingabe = input("Ihr Vorschlag: ")
+    x = 0
+    for buchstabe in rate_wort:
+        if buchstabe.lower() == nutzereingabe.lower():
+            print("Treffer")
+            erraten[x] = buchstabe
+            break
+        x += 1
+        if x >= len(rate_wort):
+            fehlversuche -= 1
+    
+    if '_' in erraten:
+        
+        print("Du hast noch ", fehlversuche)
+        if fehlversuche == 0:
+            print("Schade")
+    else:
+        print("gewonnen, das Wort war", rate_wort)
+        break
